2deb073461d329f7822251c9983dc80fab8bd0b9
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-11 00:14:06 -0700

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_Ex_List_Manjula
+{
+    internal class Student
+    {
+        string firstName;
+        string lastName;
+        double csiGrade;
+        double genEdGrade;
+
+        public Student(string firstName, string lastName, double csiGrade, double genEdGrade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.csiGrade = csiGrade;
+            this.genEdGrade = genEdGrade;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public double CsiGrade { get => csiGrade; set => csiGrade = value; }
+        public double GenEdGrade { get => genEdGrade; set => genEdGrade = value; }
+
+        public double StudentAverage()
+        {
+            throw new Exception();
+        }
+    }
+}
